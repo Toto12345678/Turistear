642c2d976ebbb3fb90a5448d8ab79c648a0dc228
JISG <israel_gutierrez3@hotmail.com>
2018-10-12 20:45:51 -0500

    Puse LaraCast en wlcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaraCast
                 </div>
 
                 <div class="links">
