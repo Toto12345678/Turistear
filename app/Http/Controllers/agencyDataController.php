dc24ed6c8121cefcc37c7cd33aabaa7993872bf9
leonardo1497 <1497enrique@gmail.com>
2018-10-13 04:48:45 -0500

    guardado imagen agencia

--- app/Http/Controllers/agencyDataController.php
+++ app/Http/Controllers/agencyDataController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\UploadedFile;
 
 use App\agencyData;
 class agencyDataController extends Controller
@@ -18,11 +19,17 @@ class agencyDataController extends Controller
 
     public function crear(Request $request)
     {
-        $file = $request->file('foto_perfil');
+        $id=Auth::user()->id;
+        $nameImgPerfil = $_FILES['foto_perfil']['name'];
+        $perfilName= $id.$nameImgPerfil;
+        $rutaPerfil= 'imgAgencia/'.$perfilName;
+        Image::make($request->file('foto_perfil'))
+        ->resize(1080,720)
+        ->save($rutaPerfil);
         agencyData::create([
-            'id_user' =>Auth::user()->id,
+            'id_user' =>$id,
             'telefono' =>$request->telefono,
-            'foto_perfil' =>$file->getClientOriginalName(),
+            'foto_perfil' =>$rutaPerfil,
             'direccion' => $request->direccion,
             'descripcion' => $request->descripcion,
             'num_cuenta'=>$request->num_cuenta,
