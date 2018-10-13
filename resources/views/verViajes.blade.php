170c585526c2f74f4934f4fea1f6d401b59aa171
leonardo1497 <1497enrique@gmail.com>
2018-10-13 11:50:22 -0500

    perfil creacion

--- resources/views/verViajes.blade.php
+++ resources/views/verViajes.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class=row>
+  <div class="col-md-12 text-center">
+    <h1>Tenemos estos paquetes para ti</h1>
+    </div>
+</div>
  <div class="row">
         @foreach($paquetes as $paquete)
-          <div class="col-md-4">
+          <div class="col-md-3 pt-5 mx-auto">
             <div class="card" style="width:100%;">
               @foreach ($fotos as $foto)
                 @if ($paquete->id == $foto->id_paquete)
@@ -24,7 +28,7 @@
                 </div>
                 <form action="{{url('perfilTransporte')}}" method="get">
                   <input type="hidden" name="usuario" value="{{$paquete->id}}">
-                  <input type="submit" class="btn boton" value="Ver perfil" style="background:#18d26e;width: 100%">
+                  <input type="submit" class="btn-primary" value="Ver perfil" style="width: 100%">
                   {{csrf_field()}}
                 </form>
               </div>
