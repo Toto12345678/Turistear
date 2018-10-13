bf68d13474beeb97d29f05b70457db2f6fb04254
leonardo1497 <1497enrique@gmail.com>
2018-10-12 22:37:57 -0500

    las bases de datos fueron creados

--- database/migrations/2018_10_11_054510_create_paquetes_table.php
+++ database/migrations/2018_10_11_054510_create_paquetes_table.php
@@ -15,6 +15,19 @@ class CreatePaquetesTable extends Migration
     {
         Schema::create('paquetes', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('id_agencia')->unsigned();
+            $table->foreign('id_agencia')->references('id')->on('users');
+            $table->string('estado_llegada');
+            $table->string('ciudad_llegada');
+            $table->string('estado_salida');
+            $table->string('ciudad_salida');
+            $table->string('nombre');
+            $table->float('precio_original')->default(0);
+            $table->float('precio_max')->default(0);
+            $table->float('precio_min')->default(0);
+            $table->string('descripcion');
+            $table->string('fecha_fin');
+            $table->integer('lugares');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2018_10_13_031730_create_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_paquete')->unsigned();
+            $table->foreign('id_paquete')->references('id')->on('paquetes');
+            $table->string('dir_img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
